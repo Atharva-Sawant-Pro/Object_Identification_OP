e997a8b9b917ca6d6f1c23b981931ffc30c1d670
Atharva-Sawant-Pro <78305129+Atharva-Sawant-Pro@users.noreply.github.com>
2021-05-15 15:40:43 +0530

    Add files via upload

---
+++ Football.js
@@ -0,0 +1,45 @@
+imag="";
+status="";
+objects=[];
+function preload(){
+    imag=loadImage("Football_img_project_WHJ-1.jpg");
+}
+function back(){
+    window.location="index.html";
+}
+function setup(){
+    canvas=createCanvas(550,420);
+    canvas.center();
+    o_d=ml5.objectDetector("cocossd",modelLoded);
+    document.getElementById("status").innerHTML="Status: Detecting Object";
+}
+function modelLoded(){
+    console.log("Moodaal Loodaad");
+    status=true;
+    o_d.detect(imag,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        objects=results;
+    }
+}
+function draw(){
+    image(imag,0,0,550,420);
+    if(status != ""){
+        for(i=0; i<objects.length; i++){
+            document.getElementById("status").innerHTML="Status: Detected Object";
+            fill("red");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+        }
+    }
+
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+function chocos(){
+    window.location="Chocos.html";
+}
+function Football(){
+    window.location="Football.html";
+}
+function knife  (){
+    window.location="bottle.html";
+}
+function computer(){
+    window.location="Computer.html";
+}
+function phone(){
+    window.location="Phone.html";
+}
